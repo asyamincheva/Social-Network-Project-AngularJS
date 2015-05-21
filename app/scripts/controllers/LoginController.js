f901588a9b06dec6b7e8506e6e1203cc42b2a81f
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-21 15:00:42 +0300

    added toaster, loginController

---
+++ app/scripts/controllers/LoginController.js
@@ -0,0 +1,34 @@
+'use strict';
+
+mySquadSocialNetwork.controller('LoginController',
+    ['$scope', '$route', '$timeout', 'userData', 'credentials', 'toaster',
+        function ($scope, $route, $timeout, userData, credentials, toaster) {
+        $scope.rememberMe = false;
+        $scope.login = login;
+
+        function login(user, loginForm) {
+            userData.login(user)
+                .$promise
+                .then(function (data) {
+                    $scope.user = {};
+
+                    if ($scope.rememberMe) {
+                        $scope.$storage = credentials.saveInLocalStorage(data.access_token, data.token_type);
+                    } else {
+                        $scope.$storage = credentials.saveInSessionStorage(data.access_token, data.token_type);
+                    }
+
+                    toaster.pop('success', 'Login successful!');
+                    $scope.loginForm.$setPristine();
+                    reloadRoute(2000);
+                }, function (error) {
+                    toaster.pop('error', 'Login unsuccessful!', error.data.message);
+                });
+        }
+        function reloadRoute(time) {
+            $timeout(function () {
+                $route.reload();
+            }, time);
+        }
+    }
+    ]);
