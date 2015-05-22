30ee8dd38ba28c2fdc06604231d7f49ce29b2177
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-23 01:32:40 +0300

    edited link in header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -48,7 +48,7 @@ var mySquadSocialNetwork = angular
                     }
                 }
             })
-            
+
             .otherwise({
                 redirectTo: '/'
             })
