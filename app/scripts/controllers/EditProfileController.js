1bb820fa59fa3d7dbfbf630b27721a57670ff02a
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-23 01:16:05 +0300

    edited edit-profile pic display

--- app/scripts/controllers/EditProfileController.js
+++ app/scripts/controllers/EditProfileController.js
@@ -4,8 +4,6 @@ mySquadSocialNetwork.controller('EditProfileController',
     ['$scope', '$location', '$timeout', 'userData', 'credentials', 'toaster', function ($scope, $location, $timeout, userData, credentials, toaster){
         $scope.editUser = credentials.getLoggedUser();
         $scope.editProfile = editProfile;
-        $scope.formatProfileImgToBase64 = formatProfileImgToBase64;
-        $scope.formatCoverImgToBase64 = formatCoverImgToBase64;
 
         function editProfile(user, editProfileForm) {
             userData.edit(user)
@@ -19,13 +17,49 @@ mySquadSocialNetwork.controller('EditProfileController',
                 })
         }
 
-        function formatProfileImgToBase64() {
-            $scope.editUser.profileImageData = 'data:image/jpg;base64,' + $scope.editUser.profileImageData.base64;
-        }
+        $scope.uploadProfileImage = function (files) {
+			var file = files[0];
+			var reader;
 
-        function formatCoverImgToBase64() {
-            $scope.editUser.coverImageData = 'data:image/jpg;base64,' + $scope.editUser.coverImageData.base64;
-        }
+			if(!file.type.match(/image\/.*/)) {
+				$('#profile-image').attr('src', '');
+
+				$scope.editUser.profileImageData = undefined;
+			} else if(file.size > 131072) {
+				$('#profile-image').attr('src', '');
+
+				$scope.editUser.profileImageData = undefined;
+			} else {
+				reader = new FileReader();
+				reader.onload = function() {
+					$('#profile-image').attr('src', reader.result);
+					$scope.editUser.profileImageData = reader.result;
+				};
+				reader.readAsDataURL(file);
+			}
+		};
+
+        $scope.uploadCoverImage = function (files) {
+			var file = files[0];
+			var reader;
+
+			if(!file.type.match(/image\/.*/)) {
+				$('#cover-image').attr('src', '');
+
+				$scope.editUser.coverImageData = undefined;
+			} else if(file.size > 131072) {
+				$('#cover-image').attr('src', '');
+
+				$scope.editUser.coverImageData = undefined;
+			} else {
+				reader = new FileReader();
+				reader.onload = function() {
+					$('#cover-image').attr('src', reader.result);
+					$scope.editUser.coverImageData = reader.result;
+				};
+				reader.readAsDataURL(file);
+			}
+		};
 
         function redirectToHome(time) {
             $timeout(function () {
