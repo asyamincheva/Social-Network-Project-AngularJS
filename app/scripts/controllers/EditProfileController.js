1ee9de6ace4b7acf6c395b890b5d5bb72233bb82
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-22 21:55:52 +0300

    implemented EditUser

---
+++ app/scripts/controllers/EditProfileController.js
@@ -0,0 +1,36 @@
+'use strict';
+
+mySquadSocialNetwork.controller('EditProfileController',
+    ['$scope', '$location', '$timeout', 'userData', 'credentials', 'toaster', function ($scope, $location, $timeout, userData, credentials, toaster){
+        $scope.editUser = credentials.getLoggedUser();
+        $scope.editProfile = editProfile;
+        $scope.formatProfileImgToBase64 = formatProfileImgToBase64;
+        $scope.formatCoverImgToBase64 = formatCoverImgToBase64;
+
+        function editProfile(user, editProfileForm) {
+            userData.edit(user)
+                .$promise
+                .then(function (data) {
+                    $scope.editProfileForm.$setPristine();
+                    toaster.pop('success', 'Edit successful!', data.message);
+                    $location.path('/home');
+                }, function (error) {
+                    toaster.pop('error', 'Edit profile error!', error.data.message);
+                })
+        }
+
+        function formatProfileImgToBase64() {
+            $scope.editUser.profileImageData = 'data:image/jpg;base64,' + $scope.editUser.profileImageData.base64;
+        }
+
+        function formatCoverImgToBase64() {
+            $scope.editUser.coverImageData = 'data:image/jpg;base64,' + $scope.editUser.coverImageData.base64;
+        }
+
+        function redirectToHome(time) {
+            $timeout(function () {
+                $location.path('/');
+            }, time);
+        }
+    }
+    ]);
