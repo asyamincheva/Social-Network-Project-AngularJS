171efd9db3f1dec4c2e3ca468ffa3d1aae0f2e2e
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-22 20:07:50 +0300

    fixed LoginController

--- app/scripts/controllers/LoginController.js
+++ app/scripts/controllers/LoginController.js
@@ -3,31 +3,40 @@
 mySquadSocialNetwork.controller('LoginController',
     ['$scope', '$route','$location', '$timeout', 'userData', 'credentials', 'toaster',
         function ($scope, $route, $location, $timeout, userData, credentials, toaster) {
-        $scope.rememberMe = false;
-        $scope.login = login;
+            $scope.rememberMe = false;
+            $scope.login = login;
 
-        function login(user, loginForm) {
-            userData.login(user)
-                .$promise
-                .then(function (data) {
-                    $scope.user = {};
+            function login(user, loginForm) {
+                userData.login(user)
+                    .$promise
+                    .then(function (data) {
+                        $scope.user = {};
 
-                    if ($scope.rememberMe) {
-                        $scope.$storage = credentials.saveTokenInLocalStorage(data.access_token, data.token_type);
-                    } else {
-                        $scope.$storage = credentials.saveTokenInSessionStorage(data.access_token, data.token_type);
-                    }
+                        if ($scope.rememberMe) {
+                            $scope.$storage = credentials.saveTokenInLocalStorage(data.access_token, data.token_type);
+                        } else {
+                            $scope.$storage = credentials.saveTokenInSessionStorage(data.access_token, data.token_type);
+                        }
 
-                    toaster.pop('success', 'Login successful!');
-                    $location.path('/home');
-                }, function (error) {
-                    toaster.pop('error', 'Login unsuccessful!', error.data.message);
-                });
+                        userData.getLoggedUserData()
+                            .$promise
+                            .then(function (data) {
+                                credentials.saveLoggedUser(data);
+                                toaster.pop('success', 'Login successful!');
+                                $location.path('/home');
+                            }, function (error) {
+                                toaster.pop('error', 'Login error!', error.data.message);
+                                credentials.deleteCredentials();
+                                $route.reload();
+                            });
+                    }, function (error) {
+                        toaster.pop('error', 'Login unsuccessful!', error.data.message);
+                    });
+            }
+            function reloadRoute(time) {
+                $timeout(function () {
+                    $route.reload();
+                }, time);
+            }
         }
-        function reloadRoute(time) {
-            $timeout(function () {
-                $route.reload();
-            }, time);
-        }
-    }
     ]);
