098abf120f1b6415a538a4b1440157af4c9f8643
asyamincheva <asya.m.mincheva@gmail.com>
2015-05-23 21:41:46 +0300

    implemented FriendsSidebarController

---
+++ app/scripts/controllers/FriendsSidebarController.js
@@ -0,0 +1,32 @@
+'use strict';
+
+mySquadSocialNetwork.controller('FriendsSidebarController',
+    ['$scope', '$routeParams', 'credentials', 'friendsData', 'toaster', 'defaultProfileImageData',
+        function ($scope, $routeParams, credentials, friendsData, toaster, defaultProfileImageData) {
+        $scope.user = credentials.getLoggedUser();
+        $scope.defaultProfileImageData = defaultProfileImageData;
+
+        if(!$routeParams.username || $routeParams.username === $scope.user.username) {
+            $scope.username = $scope.user.username;
+
+            friendsData.getLoggedUserFriendsPreview()
+                .$promise
+                .then(function (data) {
+                    $scope.totalCount = data.totalCount;
+                    $scope.friends = data.friends;
+                }, function (error) {
+                    toaster.pop('error', 'Error!', error.data.message);
+                });
+        } else {
+            $scope.username = $routeParams.username;
+            
+            friendsData.getOtherUserFriendsPreview($routeParams.username)
+                .$promise
+                .then(function (data) {
+                    $scope.totalCount = data.totalCount;
+                    $scope.friends = data.friends;
+                }, function (error) {
+                    toaster.pop('error', 'Error!', error.data.message);
+                });
+        }
+    }]);
